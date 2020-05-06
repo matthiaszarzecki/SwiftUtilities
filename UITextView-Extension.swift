d462897d9e1c79f6bf0463c1d45cba85c1eed636
matthiaszarzecki <matthias.zarzecki@oetkerdigital.com>
2020-05-06 11:20:34 +0200

    Update UITextView-Extension.swift

--- UITextView-Extension.swift
+++ UITextView-Extension.swift
@@ -3,12 +3,20 @@ import UIKit
 
 extension UITextView {
   /**
-   Checks if a string contains another string regardless of case.
-  */
-  func getFittingSize() -> CGRect {
+   *  Returns a CGRect with the correct size to fit the contents.
+   */
+  func getFittingSizeAsCGRect() -> CGRect {
     let contentSize = self.sizeThatFits(self.bounds.size)
     let viewOrigin = self.frame.origin
     let newViewRect = CGRect(x: viewOrigin.x, y: viewOrigin.y, width: contentSize.width, height: contentSize.height)
     return newViewRect
+  }
+
+  /**
+   *  Returns a CGFloat with the correct height to fit the contents.
+   */
+  func getFittingHeight() -> CGFloat {
+    let contentSize = self.sizeThatFits(self.bounds.size)
+    return contentSize.height
   }
 }
