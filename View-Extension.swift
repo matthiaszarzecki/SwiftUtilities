e2a9283ae0a2f009710178c4f87b06a317ea79b7
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2020-11-14 01:00:13 +0100

    Add backgroundcolor

--- View-Extension.swift
+++ View-Extension.swift
@@ -1,6 +1,10 @@
 import SwiftUI
 
 extension View {
+  func backgroundColor(_ color: Color) -> some View {
+    return self.background(Rectangle().foregroundColor(color))
+  }
+
   /// Adds a background-rectangle with a random color, for debugging purposes.
   func coloredDebugBackground() -> some View {
     return backgroundColor(Utilities.randomDebugColor())
